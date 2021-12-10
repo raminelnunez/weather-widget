13785ec380601f5dfaef68965d911fdf1c679460
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-10 10:14:51 -0600

    add variables with geolocation

---
+++ src/scripts/app.js
@@ -0,0 +1,12 @@
+let lon;
+let lan;
+
+if (!navigator.geolocation) {
+	console.log('Geolocation is not supported by your browser');
+} else {
+	navigator.geolocation.getCurrentPosition((position) => {
+		lat = position.coords.latitude;
+		lon = position.coords.longitude;
+		console.log(lat, lon);
+	});
+}
