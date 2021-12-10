8b9945ce57c70cb6de04328fe1875f5c5a4e6218
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-10 11:31:22 -0600

    finished getCurrentWeather function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,7 @@
 const baseURL = 'http://api.openweathermap.org/data/2.5/'
 const apiKey = 'a230cd15bf0b29b71caeacb711a2ada6';
 const kelvin = 273.15;
+let currentWeather;
 
 let lat;
 let lon;
@@ -12,7 +13,7 @@ if (!navigator.geolocation) {
 		lat = position.coords.latitude;
 		lon = position.coords.longitude;
     console.log(lat, lon);
-    getWeather();
+    getCurrentWeather();
 	});
 }
 
@@ -23,7 +24,9 @@ const getCoords = () => {
 function getCurrentWeather() {
   return fetch(`${baseURL}weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
   .then((response) => response.json())
-  .then((data) => console.log(data.main))
+  .then((data) => currentWeather = new CurrentWeather(
+    `http://openweathermap.org/img/wn/ ${data.weather[0].icon}@2x.png`, data.main.temp, data.weather[0].description))
+  .then(console.log(currentWeather))
 }
 
 class CurrentWeather {
