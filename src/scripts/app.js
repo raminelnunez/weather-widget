d09b85871396167ba2240aa9d6fb8d0797ba836a
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-11 00:04:25 -0600

    make app present daily weather info rather than present tri-hourly info as daily info

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -20,7 +20,6 @@ if (!navigator.geolocation) {
 	navigator.geolocation.getCurrentPosition((position) => {
 		lat = position.coords.latitude;
 		lon = position.coords.longitude;
-    console.log(lat, lon);
     getCurrentWeather();
     getForecast();
 	});
@@ -34,41 +33,66 @@ function getCurrentWeather() {
   return fetch(`${baseURL}weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
   .then((response) => response.json())
   .then((data) => currentWeather = new CurrentWeather(
-    `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`, data.main.temp, data.weather[0].description));
+    data.weather[0].icon, data.main.temp, data.weather[0].description));
 };
 
 function getForecast() {
   return fetch(`${baseURL}forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
   .then((response) => response.json())
-  .then((data) => parseForecast(data.list, 6))
-  .then(() => console.log(weeklyWeather))
-  .then(() => render(currentWeather, weeklyWeather));
+  .then((data) => parseForecast(data.list))
+  .then(() => render(currentWeather, weeklyWeather))
 };
 
-function parseForecast(array, howMany) {
-  for (let i = 1; i < howMany; i++) {
-    let dailyWeather = new DailyWeather(week[date.getDay() -1 + i],
-      `http://openweathermap.org/img/wn/${array[i].weather[0].icon}@2x.png`,
-      array[i].weather[0].description,
-      array[i].main.temp_max,
-      array[i].main.temp_min
-    )
-    weeklyWeather.push(dailyWeather);
+function parseForecast(array) {
+  let dates = []
+  let datesInfoArray = [[],[],[],[],[]];
+  for (let i = 0; i < 5; i++) {
+    dates[i] = (date.getDate() + i + 1)
   };
+  
+  for (let item of array) {
+    let itemDate = parseInt(item.dt_txt[8].toString() + item.dt_txt[9].toString());
+    for(let i = 0; i < dates.length; i++) {
+      if (itemDate === dates[i]) {
+        datesInfoArray[i].push(item)
+      }
+    }
+  }
+
+  let weekDayInt = date.getDay();
+  let dateInt = date.getDate();
+  for (let day of datesInfoArray) {
+    weekDayInt++
+    dateInt++
+    let minTemp = undefined;
+    let maxTemp = undefined;
+    for (let trihour of day) {
+      if (minTemp === undefined || trihour.main.temp_min < minTemp) {
+        minTemp = trihour.main.temp_min
+      }
+      if (maxTemp === undefined || trihour.main.temp_max > maxTemp) {
+        maxTemp = trihour.main.temp_max
+      }
+    }
+    let icon = day[day.length/2].weather[0].icon;
+    let description = day[day.length/2].weather[0].description;
+    weeklyWeather.push(new DailyWeather(week[weekDayInt], dateInt, icon, description, maxTemp, minTemp))
+  }
 };
 
 class CurrentWeather {
-  constructor(imageURL, temperature, description) {
-    this.image = imageURL;
+  constructor(icon, temperature, description) {
+    this.imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
     this.temp = temperature;
     this.desc = description;
   };
 };
 
 class DailyWeather {
-  constructor(day, imageURl, description, maxTemperature, minTemperature) {
+  constructor(day, date, icon, description, maxTemperature, minTemperature) {
     this.day = day;
-    this.image = imageURl;
+    this.date = date;
+    this.imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
     this.desc = description;
     this.maxTemp = maxTemperature;
     this.minTemp = minTemperature;
@@ -79,7 +103,7 @@ function render(currentWeather, weeklyWeather) {
   html.currrentConditions.innerHTML = "";
   html.currrentConditions.insertAdjacentHTML('afterbegin', `
   <h2>Current Conditions</h2>
-  <img src="${currentWeather.image}" />
+  <img src="${currentWeather.imageURL}" />
   <div class="current">
     <div class="temp">${(currentWeather.temp - kelvin).toFixed(0)}℃</div>
     <div class="condition">${currentWeather.desc}</div>
@@ -91,7 +115,7 @@ function render(currentWeather, weeklyWeather) {
     html.forecast.insertAdjacentHTML('beforeend', `
     <div class="day">
       <h3>${day.day}</h3>
-      <img src="${day.image}" />
+      <img src="${day.imageURL}" />
       <div class="description">${day.desc}</div>
       <div class="temp">
         <span class="high">${(day.maxTemp - kelvin).toFixed(0)}℃</span>/<span class="low">${(day.minTemp - kelvin).toFixed(0)}℃</span>
