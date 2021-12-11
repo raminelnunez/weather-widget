f259f8860e289214d54cb29a1963a85a64295a5e
Raminel Nunez <Raminelnc23@gmail.com>
2021-12-10 22:27:27 -0600

    add semicolons

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,15 @@
-const baseURL = 'http://api.openweathermap.org/data/2.5/'
+const baseURL = 'http://api.openweathermap.org/data/2.5/';
 const apiKey = 'a230cd15bf0b29b71caeacb711a2ada6';
 const kelvin = 273.15;
 const date = new Date();
-const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 let currentWeather;
 const weeklyWeather = [];
 
 const html = {
   currrentConditions: document.getElementsByClassName('current-conditions')[0],
   forecast: document.getElementsByClassName('forecast')[0]
-}
+};
 
 let lat;
 let lon;
@@ -24,18 +24,18 @@ if (!navigator.geolocation) {
     getCurrentWeather();
     getForecast();
 	});
-}
+};
 
 const getCoords = () => {
   return [lat, lon];
-}
+};
 
 function getCurrentWeather() {
   return fetch(`${baseURL}weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
   .then((response) => response.json())
   .then((data) => currentWeather = new CurrentWeather(
     `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`, data.main.temp, data.weather[0].description));
-}
+};
 
 function getForecast() {
   return fetch(`${baseURL}forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
@@ -43,7 +43,7 @@ function getForecast() {
   .then((data) => parseForecast(data.list, 6))
   .then(() => console.log(weeklyWeather))
   .then(() => render(currentWeather, weeklyWeather));
-}
+};
 
 function parseForecast(array, howMany) {
   for (let i = 1; i < howMany; i++) {
@@ -54,16 +54,16 @@ function parseForecast(array, howMany) {
       array[i].main.temp_min
     )
     weeklyWeather.push(dailyWeather);
-  }
-}
+  };
+};
 
 class CurrentWeather {
   constructor(imageURL, temperature, description) {
     this.image = imageURL;
     this.temp = temperature;
     this.desc = description;
-  }
-}
+  };
+};
 
 class DailyWeather {
   constructor(day, imageURl, description, maxTemperature, minTemperature) {
@@ -72,8 +72,8 @@ class DailyWeather {
     this.desc = description;
     this.maxTemp = maxTemperature;
     this.minTemp = minTemperature;
-  }
-}
+  };
+};
 
 function render(currentWeather, weeklyWeather) {
   html.currrentConditions.innerHTML = "";
@@ -84,7 +84,7 @@ function render(currentWeather, weeklyWeather) {
     <div class="temp">${(currentWeather.temp - kelvin).toFixed(0)}℃</div>
     <div class="condition">${currentWeather.desc}</div>
   </div>
-  `)
+  `);
 
   html.forecast.innerHTML = "";
   weeklyWeather.forEach((day) => {
@@ -97,6 +97,6 @@ function render(currentWeather, weeklyWeather) {
         <span class="high">${(day.maxTemp - kelvin).toFixed(0)}℃</span>/<span class="low">${(day.minTemp - kelvin).toFixed(0)}℃</span>
       </div>
     </div>
-    `)
-  })
-}
+    `);
+  });
+};
